faefcecc501e9d775b83a2d547db69d6a3c0fcf7
gogomca <gogomca@gmail.com>
2025-02-19 21:47:36 +0100

    Document SAM

--- src/main/java/es/gogomca/ocpj/lambdas/example2/FunctionalInterface1.java
+++ src/main/java/es/gogomca/ocpj/lambdas/example2/FunctionalInterface1.java
@@ -2,5 +2,5 @@ package es.gogomca.ocpj.lambdas.example2;
 
 @FunctionalInterface
 public interface FunctionalInterface1 {
-  void method();
+  void method(); // SAM - Single Abstract Method.
 }
--- src/main/java/es/gogomca/ocpj/lambdas/example2/FunctionalInterface2.java
+++ src/main/java/es/gogomca/ocpj/lambdas/example2/FunctionalInterface2.java
@@ -2,5 +2,5 @@ package es.gogomca.ocpj.lambdas.example2;
 
 @FunctionalInterface
 public interface FunctionalInterface2 extends FunctionalInterface1 {
-
+  // It is a functional interface because it inherits the SAM from FunctionalInterface1.
 }
