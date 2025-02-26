b219a3c00e8abf5f09f57e5d6d1c43085afbf9ac
gogomca <gogomca@gmail.com>
2025-02-26 20:02:00 +0100

    Document constructor method reference example

--- src/main/java/es/gogomca/ocpj/lambdas/ex8/methodreference/constructor/StringCopierApp.java
+++ src/main/java/es/gogomca/ocpj/lambdas/ex8/methodreference/constructor/StringCopierApp.java
@@ -5,6 +5,8 @@ import java.util.function.Function;
 public class StringCopierApp {
 
   public static void main(String[] args) {
+    // In accord with the SAM it is something what expect a String and return a String.
+    // String: public String(String original)
     StringCopier methodRef = String::new;
 
     var myString = methodRef.copy("Zebra");
@@ -13,6 +15,7 @@ public class StringCopierApp {
 
     // ---------------------------------------------------
 
+    // In this case we are using another approach to use the constructor method reference.
     Function<String, String> parameterisedConstructor = String::new;
 
     var otherString = parameterisedConstructor.apply("Leopard");
