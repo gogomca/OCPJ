bdca83f0e7133a96a1dd838340aaa42f14777059
gogomca <gogomca@gmail.com>
2025-03-04 21:29:59 +0100

    Add more Predicate examples

--- src/main/java/es/gogomca/ocpj/builtinfunctions/ex3/predicate/Predicates.java
+++ src/main/java/es/gogomca/ocpj/builtinfunctions/ex3/predicate/Predicates.java
@@ -14,8 +14,22 @@ public class Predicates {
     System.out.println(isEmpty.test("")); // true
     System.out.println(isEmpty.test("_")); // false
 
-    BiPredicate<String, Integer> isSameNumber = (x, y) -> Integer.valueOf(x).equals(y);
-    System.out.println(isSameNumber.test("111", 111)); // true
-    System.out.println(isSameNumber.test("111", 333)); // false
+    BiPredicate<String, String> b1 = String::startsWith; // Method reference on parameter.
+    System.out.println(b1.test("chicken", "chick")); // true
+    BiPredicate<String, String> b2 = (string, prefix) -> string.startsWith(prefix);
+    System.out.println(b2.test("chicken", "chick")); // true
+
+    BiPredicate<Integer, Long> isSameNumber1 = Integer::equals;
+    // Because the result is true if and only if the argument is not null
+    // and is an Integer object that contains the same int value as this object.
+    System.out.println(isSameNumber1.test(111, 111l)); // false
+    BiPredicate<String, Integer> isSameNumber2 = (x, y) -> Integer.valueOf(x).equals(y);
+    System.out.println(isSameNumber2.test("111", 333)); // false
+
+    BiPredicate<String, String> matchesRegex = String::matches;
+    // The BiPredicate first parameter is going to be the String where the matches() method is
+    // invoked and the BiParameter second parameter is the matches() method parameter.
+    System.out.println(matchesRegex.test("123", "\\d{3}"));// true
+    System.out.println(matchesRegex.test("12", "\\d{3}"));// false
   }
 }
